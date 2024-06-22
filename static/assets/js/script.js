660b50f52a3793b9fccf9fa667fe566712c1e906
Yushi <local@example.com>
2024-06-22 21:06:26 +0300

    Refactored to initialize other .js operations so it's the only referenced file in the HTML

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,20 +1,60 @@
-import { confirmDelete, confirmDeleteUrl, confirmDeleteAllFiles, confirmDeleteAllUrls } from './delete.js';
-import { copyText, pasteText } from './clipboard.js';
-import { saveText, loadText, clearText } from './utils.js';
+// script.js
 
-// Function to handle downloading all files
-function downloadAllFiles() {
-    window.location.href = '/download_all_files';
+// Function to dynamically load a script
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => resolve(script);
+        script.onerror = () => reject(new Error(`Script load error for ${url}`));
+        document.head.appendChild(script);
+    });
 }
 
-// Expose functions to the global scope for HTML event handlers
-window.confirmDelete = confirmDelete;
-window.confirmDeleteUrl = confirmDeleteUrl;
-window.confirmDeleteAllFiles = confirmDeleteAllFiles;
-window.confirmDeleteAllUrls = confirmDeleteAllUrls;
-window.copyText = copyText;
-window.pasteText = pasteText;
-window.saveText = saveText;
-window.loadText = loadText;
-window.clearText = clearText;
-window.downloadAllFiles = downloadAllFiles;
+// Load jQuery first
+loadScript('/static/assets/js/jquery-3.6.0.min.js')
+    .then(() => {
+        return loadScript('/static/assets/js/jquery.js');
+    })
+    .then(() => {
+        // Import other JavaScript files
+        return Promise.all([
+            import('/static/assets/js/delete.js').then(module => {
+                window.confirmDelete = module.confirmDelete;
+                window.confirmDeleteUrl = module.confirmDeleteUrl;
+                window.confirmDeleteAllFiles = module.confirmDeleteAllFiles;
+                window.confirmDeleteAllUrls = module.confirmDeleteAllUrls;
+            }),
+            import('/static/assets/js/clipboard.js').then(module => {
+                window.copyText = module.copyText;
+                window.pasteText = module.pasteText;
+                window.copyAllUrls = module.copyAllUrls;
+                window.copyUrl = module.copyUrl;
+            }),
+            import('/static/assets/js/utils.js').then(module => {
+                window.saveText = module.saveText;
+                window.loadText = module.loadText;
+                window.clearText = module.clearText;
+            })
+        ]);
+    })
+    .then(() => {
+        // Function to handle downloading all files
+        function downloadAllFiles() {
+            window.location.href = '/download_all_files';
+        }
+        window.downloadAllFiles = downloadAllFiles;
+
+        // Include jQuery functionalities
+        $(document).on("touchstart mousedown touchend mouseup", "button", function (e) {
+            e.preventDefault();
+            if (e.type === "touchstart" || e.type === "mousedown") {
+                $(this).addClass("clicked");
+            } else {
+                $(this).removeClass("clicked").blur();
+            }
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    });
