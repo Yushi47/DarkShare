32a3da54b6cda58ae59ccb9f3ffc06c2acfff620
Yushi <local@example.com>
2024-06-22 21:05:37 +0300

    Added copy all and single copy operations for the URLs

--- static/assets/js/clipboard.js
+++ static/assets/js/clipboard.js
@@ -4,30 +4,31 @@ export function copyText() {
     const textElement = document.getElementById("persistent-text");
     if (textElement) {
         textElement.select();
-        textElement.setSelectionRange(0, 99999); // For mobile devices
         document.execCommand("copy");
-        console.log('Text copied to clipboard:', textElement.value);  // Debugging line
-    } else {
-        console.error('Text element not found');
-        alert('Failed to copy text: text element not found.');
     }
 }
 
 export function pasteText() {
-    navigator.clipboard.readText()
-        .then(text => {
-            const textElement = document.getElementById("persistent-text");
-            if (textElement) {
-                textElement.value = text;
-                saveText(); // Save the pasted text
-                console.log('Text pasted from clipboard:', text);  // Debugging line
-            } else {
-                console.error('Text element not found');
-                alert('Failed to paste text: text element not found.');
-            }
+    const textElement = document.getElementById("persistent-text");
+    if (textElement) {
+        navigator.clipboard.readText().then(text => {
+            textElement.value = text;
+        }).catch(err => console.error("Failed to read clipboard contents: ", err));
+    }
+}
+
+export function copyAllUrls() {
+    fetch('/get_urls')
+        .then(response => response.json())
+        .then(urls => {
+            const textToCopy = urls.join('\n');
+            navigator.clipboard.writeText(textToCopy)
+                .catch(err => console.error("Failed to copy URLs: ", err));
         })
-        .catch(err => {
-            console.error('Failed to read clipboard contents: ', err);
-            alert('Failed to read clipboard contents.');
-        });
+        .catch(err => console.error("Failed to fetch URLs: ", err));
+}
+
+export function copyUrl(url) {
+    navigator.clipboard.writeText(decodeURIComponent(url))
+        .catch(err => console.error("Failed to copy URL: ", err));
 }
